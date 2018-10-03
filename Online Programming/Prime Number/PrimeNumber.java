7d10bb3b2ff9a60acf0035b7c02bb7720b5925f3
vinod <vinod.siragaon@tattsgroup.com>
2018-10-03 15:05:45 +1000

    Print the desired output in single line separated by spaces.

--- Online Programming/Prime Number/PrimeNumber.java
+++ Online Programming/Prime Number/PrimeNumber.java
@@ -1,5 +1,21 @@
-import java.util.Scanner;
+/*
+You are given an integer N. You need to print the series of all prime numbers till N.
 
+Input Format
+
+The first and only line of the input contains a single integer N denoting the number till where you need to find the series of prime number.
+
+Output Format
+
+Print the desired output in single line separated by spaces.
+
+Constraints
+
+1<=N<=1000
+ */
+
+
+import java.util.Scanner;
 
 class PrimeNumber {
     public static void main(String args[]) throws Exception {
